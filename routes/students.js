938cc579c94daa509e3f8ae1b9a84b67a8f943d5
aavishkarmishra <aavishkarmishra@gmail.com>
2020-10-07 13:07:44 +0530

    added  api

--- routes/students.js
+++ routes/students.js
@@ -8,48 +8,7 @@ router.route('/').get((req, res) => {
     .catch(err => res.status(400).json('Error: ' + err));
 });
 
-router.route('/add').post((req, res) => {
 
-    const username = req.body.username;
-    const email = req.body.email;
-    const rollno = req.body.rollno;
-    const classrooms = req.body.classrooms;
-  
-    const newStudent = new Student({
-      username,
-      email,
-      rollno,
-      classrooms
-    });
-  
-    newStudent.save()
-    .then(() => res.json('Student added!'))
-    .catch(err => res.status(400).json('Error: ' + err));
-  });
-  
-router.route('/delete').post((req,res) => {
-
-    const query = {
-        "email":req.body.email
-    };
-
-    Student.deleteOne(query)
-    .then(() => res.json('Student delete!'))
-    .catch(err => res.status(400).json('Error: ' + err));
-
-});
-
-
-router.route('/user').get((req,res) => {
-    const email = req.body.email;
-
-    Student.findOne({"email":email})
-    .then(user => {
-        res.json(user);
-    })
-    .catch(err => res.status(400).json('Error: ' + err));
-
-});
 
 
 module.exports = router;
--- routes/teachers.js
+++ routes/teachers.js
@@ -1,51 +1,10 @@
 const router = require('express').Router();
 let Teacher = require('../models/teacher.model');
-const Classroom = require('../models/classroom.model');
-const { json } = require('express');
 
 router.route('/').get((req, res) => {
 
   Teacher.find()
     .then(teacher => res.json(teacher))
-    .catch(err => res.status(400).json('Error: ' + err));
-});
-
-
-router.route('/add').post((req, res) => {
-
-    const username = req.body.username;
-    const email = req.body.email;
-    const classrooms = req.body.classrooms;
-  
-    const newTeacher = new Teacher({
-      username,
-      email,
-      classrooms
-    });
-  
-    newTeacher.save()
-    .then(() => res.json('Teacher added!'))
-    .catch(err => res.status(400).json('Error: ' + err));
-  });
-
-router.route('/delete').post((req,res) => {
-
-    const query = {
-        "email":req.body.email
-    };
-
-    Teacher.deleteOne(query)
-    .then(() => res.json('Teacher delete!'))
-    .catch(err => res.status(400).json('Error: ' + err));
-    });
-    
-router.route('/user').get((req,res) => {
-    const email = req.body.email;
-
-    Teacher.findOne({"email":email})
-    .then(user => {
-        res.json(user)
-    })
     .catch(err => res.status(400).json('Error: ' + err));
 });
 
